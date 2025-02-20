064e0cfa53bf7788335a3683f8cc0f6f415b3b8d
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-02-20 16:15:26 +0200

    Retrieves and Updates the task list in memory.

--- Codsoft/to-do.py
+++ Codsoft/to-do.py
@@ -1,2 +1,11 @@
 import json
 from datetime import datetime
+
+in_memory_tasks = []
+
+def load_tasks():
+    return in_memory_tasks
+
+def save_tasks(tasks):
+    global in_memory_tasks
+    in_memory_tasks = tasks
