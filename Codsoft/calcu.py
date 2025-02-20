b069163b383b10569e928341c7c2cba3646d69ac
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-02-20 23:02:09 +0200

    Print the Calculator Menu

--- Codsoft/calcu.py
+++ Codsoft/calcu.py
@@ -1,2 +1,8 @@
 def calculator():
 
+    print("Simple Calculator")
+    print("Choose an operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
