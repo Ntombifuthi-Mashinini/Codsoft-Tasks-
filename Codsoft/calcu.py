f86684b0e98a4fd0da9f8d8d26a49484f84eb928
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-02-20 23:20:42 +0200

    Added user input and Error handling

--- Codsoft/calcu.py
+++ Codsoft/calcu.py
@@ -6,3 +6,13 @@ def calculator():
     print("2. Subtraction (-)")
     print("3. Multiplication (*)")
     print("4. Division (/)")
+
+        num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /): ").strip()
+
+        try:
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
+
+
